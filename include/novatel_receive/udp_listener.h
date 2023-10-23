0e650552650a9f69c7f7be407e364a6225233155
yuyeong shin <yyshin@katech.re.kr>
2023-10-23 17:28:49 +0900

    first commit

---
+++ include/novatel_receive/udp_listener.h
@@ -0,0 +1,24 @@
+#ifndef _UDP_LISTENER_H_
+#define _UDP_LISTENER_H_
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <stdio.h>
+#include <cerrno>
+#include <cstring>
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ros/ros.h>
+#include <math.h>
+#include <can_msgs/Frame.h>
+
+#endif
---
+++ src/novatel_main.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "novatel_listener");
+    ros::NodeHandle node("~");
+
+    
+}
